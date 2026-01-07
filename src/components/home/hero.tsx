67f4342b0407b557c8b9b636569c58c7a9cafe4a
gustavoprocopio27 <gustavoprocopio727@gmail.com>
2026-01-07 12:22:03 -0300

    fix: fix the error ocurring in the curriculum download

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,4 +1,8 @@
 import foto from "/icons/utils/me.jpg"
+import { FaFileWord } from "react-icons/fa6";
+import { FaFilePdf } from "react-icons/fa6";
+
+
 import { useTranslation } from "react-i18next";
 
 export default function Hero()
@@ -26,8 +30,8 @@ export default function Hero()
                 </div>
                 <div className="flex justify-center">
                     <div className="flex flex-row bg-teal-950 text-ghostwhite font-semibold w-fit p-3 rounded-2xl gap-2">
-                        <a className="hover:text-teal-300 " href="/upload/Curriculo-Gustavo-Luiz-da-Silva-Procopio.pdf" download>PDF</a>
-                        <a className="hover:text-teal-300" href="/upload/Curriculo-Gustavo-Luiz-da-Silva-Procopio.docx" download>WORD</a>
+                        <a className="hover:text-teal-300 flex items-center" href={`${import.meta.env.BASE_URL}/upload/Curriculo-Gustavo-Luiz-da-Silva-Procopio.pdf`} download><FaFilePdf size={20}/> PDF</a>
+                        <a className="hover:text-teal-300 flex items-center" href={`${import.meta.env.BASE_URL}/upload/Curriculo-Gustavo-Luiz-da-Silva-Procopio.docx`} download><FaFileWord size={20} /> WORD</a>
                     </div>
                 </div>
             </div>
