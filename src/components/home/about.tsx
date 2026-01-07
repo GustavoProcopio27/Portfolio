8ca6297517614247d1a9405f96950ebe7ad5a047
gustavoprocopio27 <gustavoprocopio727@gmail.com>
2026-01-07 12:22:27 -0300

    feat: add the new premiations at OBMEP

--- src/components/home/about.tsx
+++ src/components/home/about.tsx
@@ -4,13 +4,18 @@ import { RiImageCircleFill } from "react-icons/ri";
 import { IoMdCloseCircle } from "react-icons/io";
 import { useTranslation } from "react-i18next";
 import certificadoHashtagPython from "/icons/utils/python_journey.png";
-import mencaoHonrosaOBMEP from "/icons/utils/obmep.png";
+import mencaoHonrosaOBMEP2024 from "/icons/utils/obmep2024.png";
+import mencaoHonrosaOBMEP2025 from "/icons/utils/obmep2025.jpg";
+import medalhaOBMEP from "/icons/utils/premiacao_obmep.png";
 
 export default function About()
 {
     const [isModalPythonJourneyVisible, setIsModalPythonJourneyVisible] = useState(false)
 
-    const [isModalOBMEPVisible, setIsModalOBMEPVisible]=useState(false)
+    const [isModalOBMEP2024Visible, setIsModalOBMEP2024Visible]=useState(false)
+    const [isModalOBMEP2025Visible, setIsModalOBMEP2025Visible] = useState(false)
+    const [isModalOBMEPMedalVisible, setIsModalOBMEPmedalVisible] = useState(false)
+
     const { t }=useTranslation()
 
     return(
@@ -87,32 +92,57 @@ export default function About()
                     <span className="text-2xl font-semibold">{t("about.certificates.text")}</span>   
                 </summary>
 
-                <div className="bg-teal-700 p-6 rounded-2xl mt-8 text-xl text-ghostwhite font-semibold transition-all duration-400  hover:bg-linear-to-r  hover:from-teal-600  hover:to-teal-700">
+                <div className="bg-teal-700 p-6 flex-col flex gap-2 rounded-2xl mt-8 text-xl text-ghostwhite font-semibold transition-all duration-400  hover:bg-linear-to-r  hover:from-teal-600  hover:to-teal-700">
+                   
                     <p className="flex flex-row justify-between items-center gap-4">
                         <span>
-                            {t("about.certificates.obmep")}
+                            {t("about.certificates.obmep2024")}
                         </span>
-                        <span><RiImageCircleFill onClick={()=>setIsModalOBMEPVisible(true)} className="size-6 hover:text-teal-300 hover:size-7 transition-all ease-in-out duration-300"/></span>
+                        <span><RiImageCircleFill onClick={()=>setIsModalOBMEP2024Visible(true)} className="size-6 hover:text-teal-300 hover:size-7 transition-all ease-in-out duration-300"/></span>
+                    </p>
+
+                     <p className="flex flex-row justify-between items-center gap-4">
+                        <span>
+                            {t("about.certificates.obmep2025")}
+                        </span>
+                        <span><RiImageCircleFill onClick={() => setIsModalOBMEP2025Visible(true)} className="size-6 hover:text-teal-300 hover:size-7 transition-all ease-in-out duration-300" /></span>
+                    </p>
+
+                    <p className="flex flex-row justify-between items-center gap-4">
+                        <span>
+                            {t("about.certificates.premiacaoOBMEP2025")}
+                        </span>
+                        <span><RiImageCircleFill onClick={() => setIsModalOBMEPmedalVisible(true)} className="size-6 hover:text-teal-300 hover:size-7 transition-all ease-in-out duration-300" /></span>
                     </p>
 
                     <p className="flex flex-row justify-between items-center gap-4">
                         <span>
                             {t("about.certificates.hashtag")}
                         </span>
-                            <span><RiImageCircleFill onClick={() => setIsModalPythonJourneyVisible(true)}  className="size-6 hover:text-teal-300 hover:size-7 transition-all ease-in-out duration-300"/></span>
-    
+                        <span><RiImageCircleFill onClick={() => setIsModalPythonJourneyVisible(true)}  className="size-6 hover:text-teal-300 hover:size-7 transition-all ease-in-out duration-300"/></span>
                     </p>
 
                 </div>
 
             </details>
 
-                <div className={`${isModalOBMEPVisible ? "flex flex-col" : "hidden"} gap-8 z-50 top-50 right-140 p-8 rounded-2xl fixed bg-[rgba(141,250,214,0.7)] backdrop-blur-xs`}>
-                    <div><IoMdCloseCircle className="text-teal-950 hover:text-teal-600" size={35} onClick={() => setIsModalOBMEPVisible(false)} /></div>
-                    <div><img className="w-3xl h-xl border-2 border-teal-900 rounded-2xl" src={mencaoHonrosaOBMEP} alt="menção honrosa" /></div>
+                <div className={`${isModalOBMEP2024Visible ? "flex flex-col" : "hidden"} gap-8 z-50 top-1/10 right-1/5 p-8 rounded-2xl fixed bg-[rgba(141,250,214,0.7)] backdrop-blur-xs`}>
+                    <div><IoMdCloseCircle className="text-teal-950 hover:text-teal-600" size={35} onClick={() => setIsModalOBMEP2024Visible(false)} /></div>
+                    <div><img className="w-3xl h-xl border-2 border-teal-900 rounded-2xl" src={mencaoHonrosaOBMEP2024} alt="menção honrosa" /></div>
                 </div>
 
-                <div className={`${isModalPythonJourneyVisible ? "flex flex-col" : "hidden"} gap-8 z-50 top-50 right-140 p-8 rounded-2xl fixed bg-[rgba(141,250,214,0.7)] backdrop-blur-xs`}>
+                <div className={`${isModalOBMEP2025Visible ? "flex flex-col" : "hidden"} gap-8 z-50 top-1/10 right-1/5 p-8 rounded-2xl fixed bg-[rgba(141,250,214,0.7)] backdrop-blur-xs`}>
+                    <div><IoMdCloseCircle className="text-teal-950 hover:text-teal-600" size={35} onClick={() => setIsModalOBMEP2025Visible(false)} /></div>
+                    <div><img className="w-3xl h-xl border-2 border-teal-900 rounded-2xl" src={mencaoHonrosaOBMEP2025} alt="menção honrosa" /></div>
+                </div>
+
+
+                <div className={`${isModalOBMEPMedalVisible ? "flex flex-col" : "hidden"} gap-8 z-50 top-1/10 right-1/5 p-8 rounded-2xl fixed bg-[rgba(141,250,214,0.7)] backdrop-blur-xs`}>
+                    <div><IoMdCloseCircle className="text-teal-950 hover:text-teal-600" size={35} onClick={() => setIsModalOBMEPmedalVisible(false)} /></div>
+                    <div><img className="w-3xl h-xl border-2 border-teal-900 rounded-2xl" src={medalhaOBMEP} alt="bronze" /></div>
+                </div>
+
+                <div className={`${isModalPythonJourneyVisible ? "flex flex-col" : "hidden"} gap-8 z-50 top-1/10 right-1/5 p-8 rounded-2xl fixed bg-[rgba(141,250,214,0.7)] backdrop-blur-xs`}>
                     <div><IoMdCloseCircle className="text-teal-950 hover:text-teal-600" size={35} onClick={() => setIsModalPythonJourneyVisible(false)} /></div>
                     <div><img className="w-3xl h-xl border-2 border-teal-900 rounded-2xl" src={certificadoHashtagPython} alt="certificado python" /></div>
                 </div>
